12cab694050792b49ea713fad4226ad48d942722
Aladin147 <134957975+Aladin147@users.noreply.github.com>
2025-04-16 03:03:58 +0100

    Fix booking_detail_screen_adapter.dart

--- lib/screens/booking_panel/booking_detail_screen_adapter.dart
+++ lib/screens/booking_panel/booking_detail_screen_adapter.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../models/models.dart';
-import 'booking_detail_screen.dart';
 import 'booking_detail_screen_v2.dart';
-import 'booking_panel_controller.dart';
 import 'booking_panel_controller_v2.dart';
 
 /// BookingDetailScreenAdapter
@@ -20,18 +18,10 @@ class BookingDetailScreenAdapter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (controller is BookingPanelControllerV2) {
-      return BookingDetailScreenV2(
-        booking: booking,
-        controller: controller as BookingPanelControllerV2,
-      );
-    } else if (controller is BookingPanelController) {
-      return BookingDetailScreen(
-        booking: booking,
-        controller: controller as BookingPanelController,
-      );
-    } else {
-      throw ArgumentError('Unsupported controller type: ${controller.runtimeType}');
-    }
+    // For now, we'll just use the V2 screen for all controllers
+    return BookingDetailScreenV2(
+      booking: booking,
+      controller: controller as BookingPanelControllerV2,
+    );
   }
 }
